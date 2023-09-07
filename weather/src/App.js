4b528d7f21cb1a9efed9270751af6eed2cd15898
vincenttommi <vincenttommi@gmail.com>
2023-09-07 16:23:46 +0300

    getting locations from api

--- weather/src/App.js
+++ weather/src/App.js
@@ -6,15 +6,16 @@ import Inputs from './Components/Inputs';
 import TimeAndLocation from './Components/TimeAndLocation';
 import TemperatureAndDetails from './Components/TemperatureAndDetails';
 import Forecast from './Components/Forecast';
-import getWeatherData from './Components/Services/WeatheService';
+import getFormattedWeatherData from './Components/Services/WeatheService';
 
 function App() {
 
-const fetcWeather = async () =>{
-  const data  = await getWeatherData('weather', {q: "Dubai"});
+const fetchWeather = async () => {
+  const data  = await getFormattedWeatherData({ q: "Dubai"});
   console.log(data);
 };
-fetcWeather();
+
+fetchWeather();
 
   return (
   <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-r from-cyan-700 to-blue-700 h-fit shadow-xl  '>
--- weather/src/Components/Services/WeatheService.js
+++ weather/src/Components/Services/WeatheService.js
@@ -1,19 +1,132 @@
+// const API_KEY = '4ddbd8f50b6d34b97190c675f9a92705';
+// const BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+
+// const getWeatherData = (infoType, searchParams)=> {
+
+// const  url  = new URL(BASE_URL + "/" + infoType);
+// url.search = new URLSearchParams({...searchParams, appid:API_KEY});
+ 
+
+
+  
+//     return fetch(url)
+//     .then((res)=> res.json())
+    
+// };
+// //destrucring array 
+// const formattedCurrentWeather  = (data) => {
+//     const  {
+//         coord: {lat, lon},
+//         main: {temp, feels_like, temp_min, temp_max,humidity},
+//         name,
+//         dt,
+//         sys:{country, sunrise, sunset},
+//         weather,
+//         wind:{speed}
+//     } = data
+
+//     const {main: details, icon} = weather[0];
+  
+//    return {lat, lon, temp, feels_like,temp_min,temp_max,humidity,name,dt,country,sunrise,sunset,details, icon,speed}
+// };
+
+// const getFormattedWeatherData = async (searchParams) => {
+
+//     const formattedCurrentWeather = await  getWeatherData("weather",
+//     searchParams)
+//     .then(formattedCurrentWeather)
+
+//     return formattedCurrentWeather;
+
+// }
+
+
+// export  default getFormattedWeatherData;
+
 const API_KEY = '4ddbd8f50b6d34b97190c675f9a92705';
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
 
-const getWeatherData = (infoType, searchParams)=> {
+const getWeatherData = async (infoType, searchParams) => {
+  const url = new URL(`${BASE_URL}/${infoType}`);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
-const  url  = new URL(BASE_URL + "/" + infoType);
-url.search = new URLSearchParams({...searchParams, appid:API_KEY});
- 
+  const response = await fetch(url);
+  const data = await response.json();
 
+  return data;
+};
 
-     console.log(url);
-    return fetch(url)
-    .then((res)=> res.json())
-    .then((data)=> data);
+const formattedCurrentWeather = (data) => {
+  const {
+    coord: { lat, lon },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    name,
+    dt,
+    sys: { country, sunrise, sunset },
+    weather,
+    wind: { speed },
+  } = data;
+
+  const { main: details, icon } = weather[0];
+
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    name,
+    dt,
+    country,
+    sunrise,
+    sunset,
+    details,
+    icon,
+    speed,
+  };
 };
 
 
-export default getWeatherData;
+const formattedForecastWeather = (data) => {
+    let {timezone, dialy, hourly} = data;
+    
+    dialy  = dialy.slice(1,6).map(d => {
+
+        return{
+
+            title : formatToLocalTime(d.dt,timezone,'hh:mm a')
+        }
+    })
+}
+
+const getFormattedWeatherData = async (searchParams) => {
+  try {
+    const data = await getWeatherData("weather", searchParams);
+    const formattedData = formattedCurrentWeather(data);
+
+    const {lat, lon} = formattedCurrentWeather
+
+    const formattedForecastWeather = await getWeatherData('onecall',
+    {
+        lat, 
+        lon,
+         exclude: 'current, minutely,alerts',
+         units:searchParams.units,
+    })
+    .then(formattedForecastWeather)
+
+    return formattedData;
+  } catch (error) {
+    throw new Error("Failed to fetch weather data");
+  }
+};
+
+
+
+const formatToLocalTime  = (secs, zone,format = "ccc, dd LLL yyy' | Local time:' hh: mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format)
+
+export default getFormattedWeatherData;
